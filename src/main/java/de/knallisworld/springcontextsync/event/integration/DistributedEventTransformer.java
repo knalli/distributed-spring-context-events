3638f4f904547302f36cb84b8f29a92d81dcd33e
knalli <knallisworld@googlemail.com>
2013-04-02 22:40:35 +0200

    Transform transformer
    * change DistributedEventTransformer to a bean w/ declared methods
    * use the type auto discovery for transforming (no explicit toDTO, fromDTO any more)

--- src/main/java/de/knallisworld/springcontextsync/event/integration/DistributedEventTransformer.java
+++ src/main/java/de/knallisworld/springcontextsync/event/integration/DistributedEventTransformer.java
@@ -2,15 +2,19 @@ package de.knallisworld.springcontextsync.event.integration;
 
 import de.knallisworld.springcontextsync.event.DistributedEvent;
 import de.knallisworld.springcontextsync.event.DistributedEventDTO;
+import org.springframework.integration.annotation.Transformer;
+import org.springframework.stereotype.Component;
 
 /**
  * This transformer provides both ways of transforming an event between the internal pojo and the external dto object.
  */
+@Component
 public class DistributedEventTransformer {
 
 	private static final String DEFAULT_SOURCE = "DEFAULT_SOURCE";
 
-	public static DistributedEventDTO toDTO(final DistributedEvent event) {
+	@Transformer
+	public DistributedEventDTO toDTO(final DistributedEvent event) {
 		final DistributedEventDTO dto = new DistributedEventDTO();
 		dto.setAction(event.getAction());
 		dto.setUserId(event.getUserId());
@@ -20,7 +24,8 @@ public class DistributedEventTransformer {
 		return dto;
 	}
 
-	public static DistributedEvent fromDTO(final DistributedEventDTO dto) {
+	@Transformer
+	public DistributedEvent fromDTO(final DistributedEventDTO dto) {
 		final DistributedEvent event = new DistributedEvent(DEFAULT_SOURCE, dto.getAction(), dto.getCategory(), dto.getUserId());
 		event.setApplicationId(dto.getApplicationId());
 		event.setOriginalTimestamp(dto.getTimestamp());
